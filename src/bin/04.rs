482683f702d84efc38307f74900b85b44ca0e352
Andrew Shulaev <ash.drone@gmail.com>
2022-12-05 22:17:57 +0000

    Solve 4A

---
+++ src/bin/04.rs
@@ -0,0 +1,32 @@
+#[derive(Debug)]
+struct Interval {
+    begin: u64,
+    end: u64
+}
+
+impl Interval {
+    fn from_str(s: &str) -> Self {
+        let pair: Vec<&str> = s.split("-").collect();
+        Interval { begin: pair[0].parse().unwrap(), end: pair[1].parse().unwrap() }
+    }
+
+    fn fully_contains(&self, other: &Self) -> bool {
+        other.begin >= self.begin && other.end <= self.end
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let file = std::fs::read_to_string("data/04.txt")?;
+    let mut count: u64 = 0;
+    for line in file.lines() {
+        let pair: Vec<&str> = line.split(",").collect();
+        let i1 = Interval::from_str(pair[0]);
+        let i2 = Interval::from_str(pair[1]);
+
+        if i1.fully_contains(&i2) || i2.fully_contains(&i1) {
+            count += 1;
+        }
+    }
+    println!("{}", count);
+    Ok(())
+}
