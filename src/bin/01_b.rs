6d6606d92703753e3219055a2646cc7be845a36e
Andrew Shulaev <ash.drone@gmail.com>
2022-12-05 20:55:09 +0000

    Solve 1B

--- src/bin/01_b.rs
+++ src/bin/01_b.rs
@@ -1,17 +1,18 @@
 fn main() -> std::io::Result<()> {
     let file = std::fs::read_to_string("data/01-a.txt")?;
-    let mut curr_sum: u64 = 0;
-    let mut result: u64 = 0;
+    let mut curr_sum: i64 = 0;
+    let mut sums: Vec<i64> = Vec::new();
     for line in file.lines() {
         if line.is_empty() {
-            result = result.max(curr_sum);
+            sums.push(curr_sum);
             curr_sum = 0;
         }
         else {
-            let x: u64 = line.parse().unwrap();
+            let x: i64 = line.parse().unwrap();
             curr_sum += x;
         }
     }
-    println!("{}", result);
+    sums.sort_by_key(|x| -x);
+    println!("{}", sums[0] + sums[1] + sums[2]);
     Ok(())
 }
