e84d23e9a1e7a384f1fb04cf0d29c91ec48fe22b
Andrew Shulaev <ash.drone@gmail.com>
2022-12-05 20:52:27 +0000

    1A solved

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,3 +1,17 @@
-fn main() {
-    print!("Test")
+fn main() -> std::io::Result<()> {
+    let file = std::fs::read_to_string("data/01-a.txt")?;
+    let mut curr_sum: u64 = 0;
+    let mut result: u64 = 0;
+    for line in file.lines() {
+        if line.is_empty() {
+            result = result.max(curr_sum);
+            curr_sum = 0;
+        }
+        else {
+            let x: u64 = line.parse().unwrap();
+            curr_sum += x;
+        }
+    }
+    println!("{}", result);
+    Ok(())
 }
